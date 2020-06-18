65b930419661a59ca20090e5026470fa690f3b75
Gowtham Chowdary <42214663+GowthamChowdary@users.noreply.github.com>
2020-06-18 20:21:37 +0530

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,18 @@
+import webbrowser
+import pyautogui as py
+import time
+from datetime import datetime
+meetid=input("What's the meeting id?  ")
+when=input("Enter the date and time in the format : 'month/day/year:hour:min:sec' eg- 06/18/20:19:30  ")
+while True:
+	now=datetime.now()
+	current_time = now.strftime("%D:%H:%M")       
+	if current_time==when:
+		webbrowser.open("https://zoom.us/wc/join/"+meetid)
+		time.sleep(3)
+		py.moveTo(1054,575) #modify this line if your screen isn't big(or if the code doesn't work)
+		time.sleep(2)
+		py.click()
+		print("you're in m8")
+		break
+
